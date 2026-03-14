5d6a846c2fdea42d5a0fa2a0fdaa4b8d7cd18ed6
imtdot <space@imtdot.kr>
2026-03-14 11:38:54 +0900

    Create init.py

---
+++ littlePrint/init.py
@@ -0,0 +1,15 @@
+"""
+littlePrint - A tiny print utility for Python
+"""
+
+from .core import pr, py, pv, lprint
+
+__version__ = "1.0.0"
+__author__ = "Your Name"
+__all__ = ["pr", "py", "pv", "lprint"]
+
+# Import all by default
+pr = pr
+py = py
+pv = pv
+lprint = lprint
